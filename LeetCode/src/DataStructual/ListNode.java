1dbc93b77d494719e1fe8955d78f3b283bddefbe
SamanthaChen <chenximan@gmail.com>
2016-03-12 16:17:55 +0800

    创建的LeetCode java工程

---
+++ LeetCode/src/DataStructual/ListNode.java
@@ -0,0 +1,11 @@
+package DataStructual;
+/**
+ * Definition for singly-linked list.
+ * 单链表结构，为了方便属性方法都设为public
+ * */
+
+public class ListNode {
+	public int val;
+	public ListNode next;
+	public ListNode(int x){ val = x;}
+}

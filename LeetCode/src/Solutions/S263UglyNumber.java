f2ca213964f5e3644a656cc279dce27759a39689
SamanthaChen <chenximan@gmail.com>
2016-03-23 09:35:09 +0800

    判断丑数（添加递归）

--- LeetCode/src/Solutions/S263UglyNumber.java
+++ LeetCode/src/Solutions/S263UglyNumber.java
@@ -11,6 +11,14 @@ public class S263UglyNumber {
         while(num%5 == 0) num = num/5;//能被5整除
         //已经除完了所有可能,结果是1说明已经被2,3,5整除，否则返回假
         return num == 1;
+        
+        //递归
+//        if(num<=0)return false;
+//        if(num==1)return true;
+//        if(num%2==0) return isUgly(num/2);
+//        if(num%3==0) return isUgly(num/3);
+//        if(num%5==0) return isUgly(num/5);
+//        return false;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

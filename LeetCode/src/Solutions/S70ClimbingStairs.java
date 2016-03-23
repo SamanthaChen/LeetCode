1261a70a7650b132acd01a2b55cc8f719b105079
SamanthaChen <chenximan@gmail.com>
2016-03-23 09:27:54 +0800

    一维动态规划爬楼梯

---
+++ LeetCode/src/Solutions/S70ClimbingStairs.java
@@ -0,0 +1,24 @@
+package Solutions;
+
+public class S70ClimbingStairs {
+    public int climbStairs(int n) {
+    //(用动态规划)
+      if(n <= 0 ) return 0;
+      if(n == 1) return 1;
+      if(n == 2) return 2;
+      int[] ways = new int[n];
+      ways[0] = 1;
+      ways[1] = 2;
+      for(int i = 2; i <n ; i++ ){
+          ways[i] = ways[i-1]+ways[i-2];
+      }
+      return ways[n-1];
+  
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
